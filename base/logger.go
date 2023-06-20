680fc77662cea273f6eabd69a90c611b61bf6e25
Tije Kusnadi <tijekusnadi05@gmail.com>
2023-06-20 21:18:37 +0700

    feat(logger): add zap logger

---
+++ base/logger.go
@@ -0,0 +1,24 @@
+package base
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger struct {
+	Zap *zap.SugaredLogger
+}
+
+func NewLogger() Logger {
+	config := zap.NewDevelopmentConfig()
+
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	logger, _ := config.Build()
+
+	sugar := logger.Sugar()
+
+	return Logger{
+		Zap: sugar,
+	}
+}
